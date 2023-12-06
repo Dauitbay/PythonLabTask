121019205d94440ab524f78ca26861f25544493f
“firstname <dawitsarsenbaev@gmail.com>
2023-12-06 14:22:06 +0500

    Create Post, Get, Delete and having issue with delete

--- for_test.py
+++ for_test.py
@@ -1,9 +1,31 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
 import json
 import datetime
 import uuid
-import threading
-from main_reddit_parser import logger
+# import threading
+# import subprocess
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+prog_start_time = datetime.datetime.now().strftime("%Y%m%d")
+
+
+def write_to_file(unique_id, data):
+    with open(f"reddit-{prog_start_time}.txt", "a+") as file:
+        file.write(unique_id + ";")
+        file.write(";".join(map(str, data)) + "\n")
+
+
+def read_data_from_file():
+    # Read data from the file and return it as a list
+    data = []
+    try:
+        with open(os.path.join(BASE_DIR, f"reddit-{prog_start_time}.txt"), "r") as file:
+            for line in file:
+                data.append(line.strip().split(';'))
+    except FileNotFoundError:
+        return None
+    return data
 
 
 class RequestHandler(BaseHTTPRequestHandler):
@@ -13,42 +35,80 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', content_type)
         self.end_headers()
 
-    def write_to_file(self, unique_id, data):
-        prog_start_time = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M")
-        with open(f"reddit-{prog_start_time}.txt", "a+") as file:
-            file.write(unique_id + ";")
-            file.write(";".join(map(str, data)) + "\n")
-
     def do_POST(self):
-        if self.path == '/posts':
+        if self.path == '/posts/':
             content_length = int(self.headers['Content-Length'])
             post_data = self.rfile.read(content_length)
-            try:
-                decoded_data = json.loads(post_data.decode())
+            if os.path.exists(os.path.join(BASE_DIR, f"reddit-{prog_start_time}.txt")):
+                data_read = read_data_from_file()
+                existing_ids = {item[0] for item in data_read}
+
                 unique_id = uuid.uuid1().hex[:32]
-                self.write_to_file(unique_id, decoded_data)
-                self.set_headers(201)
-                self.wfile.write(json.dumps({'UNIQUE_ID': unique_id}).encode())
-            except json.JSONDecodeError as e:
-                self.set_headers(400)
-                self.wfile.write(json.dumps({'error': f'Error decoding JSON: {str(e)}'}).encode())
+                if unique_id not in existing_ids:
+                    decoded_data = json.loads(post_data.decode())
+                    write_to_file(unique_id, decoded_data)
+                    self.set_headers(201)
+                    self.wfile.write(json.dumps({'UNIQUE_ID': len(existing_ids)}).encode())
+                else:
+                    # Duplicate UNIQUE_ID, return a 409 (Conflict) status
+                    self.set_headers(409)
+                    self.wfile.write(json.dumps({'error': 'Duplicate UNIQUE_ID'}).encode())
+            else:
+                unique_id = uuid.uuid1().hex[:32]
+                decoded_data = json.loads(post_data.decode())
+                write_to_file(unique_id, decoded_data)
+
         else:
             self.set_headers(404)
             self.wfile.write(json.dumps({'error': 'Endpoint not found'}).encode())
 
+    def do_GET(self):
+        if self.path == '/posts/':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+
+            data = read_data_from_file()
+            response = {'data': data}
+            self.wfile.write(json.dumps(response).encode('utf-8'))
+
+        else:
+            self.set_headers(404)
+            self.wfile.write(json.dumps({'error': 'Endpoint not found'}).encode())
+
+    def do_DELETE(self):
+        if self.path.startswith('/posts/'):
+            matching_files = [f for f in os.listdir(BASE_DIR) if f.startswith("reddit-")]
+            if not matching_files:
+                self.set_headers(404)
+                self.wfile.write(json.dumps({'error': 'No matching files found'}).encode())
+                return
+
+            unique_id = self.path.split('/')[2]
+            filename = os.path.join(BASE_DIR, matching_files[0])
+            # Read data from the file
+            data = read_data_from_file()
+            # Filter out the data with the specified unique_id
+            data = [post for post in data if post[0] != unique_id]
+            # Write the modified data back to the file
+            with open(filename, "w") as file:
+                for item in data:
+                    file.write(";".join(map(str, item)) + "\n")
+
+                # Respond with a 204 status code
+            self.set_headers(204)
+            self.wfile.write(json.dumps({'message': 'Resource deleted successfully'}).encode())
+
+        else:
+            self.set_headers(404)
+            self.wfile.write(json.dumps({'error': 'Endpoint not found'}).encode())
 
 def run_server():
     server_address = ('', 8087)
     httpd = HTTPServer(server_address, RequestHandler)
-    logger.info("Server is running on http://localhost:8087")
+    print(f"Server is running on http://localhost:{server_address[1]}")
     httpd.serve_forever()
-    server_thread = threading.Thread(target=httpd.serve_forever)
-    server_thread.start()
 
 
 if __name__ == "__main__":
     run_server()
-
-
-
-
--- main_reddit_parser.py
+++ main_reddit_parser.py
@@ -13,9 +13,9 @@
     And saves them using RESTful service in reddit-YYYYMMDDHHMM.txt giving UNIQUE_ID
     to every USER COLLECTED DATA.
     AROUND 8-9 minutes required to gather 100 user data.
-    This module has additional <conts.py> module for CONSTANTS
+    This module has additional <consts.py> module for CONSTANTS
 """
-from for_test import run_server
+
 from time import sleep
 from random import randint
 import argparse
@@ -27,9 +27,9 @@ import requests
 from bs4 import BeautifulSoup
 
 from consts import (REDDIT_WEBPAGE_ADDRESS, HTML_PARSER, SHREDDIT_APP, SHREDDIT_POST, SOUP_FIND_CLASS_NAME,
-REQUEST_HEADERS, GET_POST_DATA_FINDALL_CLASS, NUMBER_OF_POSTS_NEEDED_TO_GET, LOG_FILE_NAME,
-PERIOD_COMMAND_LINE, CATEGOTY_COMMAND_LINE, AUTHOR_PROFILE_FIND,
-AUTHOR_PROFILE_FIND_CLASS_TEXT_12, AUTHOR_PROFILE_FIND_CLASS_TEXT_14, conn, server_headers)
+                    REQUEST_HEADERS, GET_POST_DATA_FINDALL_CLASS, NUMBER_OF_POSTS_NEEDED_TO_GET,
+                    PERIOD_COMMAND_LINE, CATEGOTY_COMMAND_LINE, AUTHOR_PROFILE_FIND, LOG_FILE_NAME,
+                    AUTHOR_PROFILE_FIND_CLASS_TEXT_12, AUTHOR_PROFILE_FIND_CLASS_TEXT_14, conn, server_headers)
 import http.client
 import json
 
@@ -43,9 +43,9 @@ logger = logging.getLogger("scraper")
 
 
 def delete_reddit_and_my_logfile():
-    for fname in os.listdir("."):
-        if fname.startswith(("reddit-", "my_logfile")):
-            os.remove(fname)
+    for file_name in os.listdir("."):
+        if file_name.startswith(("reddit-", "my_logfile.log")):
+            os.remove(file_name)
 
 
 # Finding URL of next page and send it to get_remaining_posts_num func. through main func.
@@ -80,32 +80,34 @@ def does_post_has_restrictions(soup: BeautifulSoup):
     soup_author_profile = BeautifulSoup(author_profile_request.text, HTML_PARSER)
     author_profile = soup_author_profile.find(AUTHOR_PROFILE_FIND)
     if author_profile is None:
-        logger.info(" Author is BLOCKED or HTML atribute is changed. Could not access the author's profile.")
+        logger.info(" Author is BLOCKED or HTML attribute is changed. Could not access the author's profile.")
         return True
     return False
 
 
 def send_data_to_server(data):
+    print(data)
+    if not data:
+        logger.warning("Attempted to send empty data to the server.")
+        return
+
+    connect = http.client.HTTPConnection('localhost', 8087, timeout=10)
+    ser_headers = {'Content-type': 'application/json'}
     try:
-        # conn = http.client.HTTPConnection('localhost', 8087, timeout=10)
-        # server_headers = {'Content-type': 'application/json'}
         encoded_data = json.dumps(data).encode('utf-8')
-
-        conn.request('POST', '/posts', body=encoded_data, headers=server_headers)
-        response = conn.getresponse()
-
+        connect.request('POST', '/posts/', body=encoded_data, headers=ser_headers)
+        response = connect.getresponse()
         logger.info("Response to POST: {}".format(response.read().decode('utf-8')))
     except http.client.RemoteDisconnected as e:
         logger.error(f"RemoteDisconnected: {e}")
     except Exception as e:
         logger.error(f"Error sending data to server: {e}")
     finally:
-        conn.close()
+        connect.close()
 
 
 # Gathering required data from posts and returning num_of_posts
 def get_remaining_posts_num(posts_url: str, number_of_posts: int):
-
     try:
         result = requests.get(url=posts_url, headers=REQUEST_HEADERS, timeout=10)
     except requests.exceptions.Timeout:
@@ -145,25 +147,31 @@ def get_remaining_posts_num(posts_url: str, number_of_posts: int):
         for karma in find_post_karma:
             try:
                 if soup_author_profile.find('p',
-                                            class_=AUTHOR_PROFILE_FIND_CLASS_TEXT_12).get_text(strip=True) == 'Post Karma':
+                                            class_=AUTHOR_PROFILE_FIND_CLASS_TEXT_12).get_text(
+                    strip=True) == 'Post Karma':
                     author_comment_karma = karma.find('span', {'data-testid': 'karma-number'}).get_text(strip=True)
                     # Add <<comment karma>> when text-12 in class
                     temp_hold_user_data.append(str(author_comment_karma))
                 elif soup_author_profile.find('p',
-                                            class_=AUTHOR_PROFILE_FIND_CLASS_TEXT_12).get_text(strip=True) == 'Comment Karma':
-                    author_karma = soup_author_profile.find('span', {'data-testid': 'karma-number'}).get_text(strip=True)
+                                              class_=AUTHOR_PROFILE_FIND_CLASS_TEXT_12).get_text(
+                    strip=True) == 'Comment Karma':
+                    author_karma = soup_author_profile.find('span', {'data-testid': 'karma-number'}).get_text(
+                        strip=True)
                     # Add <<post karma>> when text-12 in class
                     temp_hold_user_data.append(str(author_karma))
                     break
             except:
                 if soup_author_profile.find('p',
-                                            class_=AUTHOR_PROFILE_FIND_CLASS_TEXT_14).get_text(strip=True) == 'Post Karma':
+                                            class_=AUTHOR_PROFILE_FIND_CLASS_TEXT_14).get_text(
+                    strip=True) == 'Post Karma':
                     author_comment_karma = karma.find('span', {'data-testid': 'karma-number'}).get_text(strip=True)
                     # Add <<comment karma>> when text-14 in class
                     temp_hold_user_data.append(str(author_comment_karma))
                 elif soup_author_profile.find('p',
-                                            class_=AUTHOR_PROFILE_FIND_CLASS_TEXT_14).get_text(strip=True) == 'Comment Karma':
-                    author_karma = soup_author_profile.find('span', {'data-testid': 'karma-number'}).get_text(strip=True)
+                                              class_=AUTHOR_PROFILE_FIND_CLASS_TEXT_14).get_text(
+                    strip=True) == 'Comment Karma':
+                    author_karma = soup_author_profile.find('span', {'data-testid': 'karma-number'}).get_text(
+                        strip=True)
                     # Add <<post karma>> when text-14 in class
                     temp_hold_user_data.append(str(author_karma))
                     break
@@ -180,8 +188,6 @@ def get_remaining_posts_num(posts_url: str, number_of_posts: int):
 
         send_data_to_server(temp_hold_user_data)
 
-        # write_to_file(temp_hold_user_data, file_name)
-        # Exiting the function after collecting the required amount of data:
         if number_of_posts == 0:
             logger.info(NUMBER_OF_POSTS_NEEDED_TO_GET + str(number_of_posts))
             return number_of_posts
@@ -190,12 +196,11 @@ def get_remaining_posts_num(posts_url: str, number_of_posts: int):
 
 
 def parse_command_line_arg():
-   
     parser = argparse.ArgumentParser(description="Reddit Scraper")
-    parser.add_argument("--posts", default=100, type=int, help="Number of posts to scrape")
+    parser.add_argument("--posts", default=10, type=int, help="Number of posts to scrape")
     parser.add_argument("--category", default='top', type=str, help=f"Category of posts {CATEGOTY_COMMAND_LINE}")
     parser.add_argument("--period", default='month', type=str, help=f"Select period {PERIOD_COMMAND_LINE}")
-    args = parser.parse_args() 
+    args = parser.parse_args()
     try:
         if args.posts <= 0:
             raise ValueError("Number of posts must be a positive integer and higher than 0 ")
@@ -220,7 +225,7 @@ def main():
     if os.path.isfile(LOG_FILE_NAME):
         logging.shutdown()
 
-    # delete_reddit_and_my_logfile()
+    delete_reddit_and_my_logfile()
 
     while True:
         remaining_posts_count = get_remaining_posts_num(main_url, number_of_posts)
@@ -235,4 +240,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
